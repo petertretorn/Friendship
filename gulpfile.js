4070926d9390296228c52794cd1012eed51efd90
Peter Larsen <petertretorn@gmail.com>
2015-11-23 00:14:34 +0100

    gulpfile added

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+var gulp = require('gulp');
+var inject = require('gulp-inject');
+
+var paths = {
+    appScripts: 'public/app/**/*.js'
+};
+
+gulp.task('injectjs', function(){
+    var target = gulp.src('./server/views/index.ejs');
+    var sources = gulp.src([paths.appScripts]);
+
+    return target.pipe(inject(sources, {relative: true}))
+        .pipe(gulp.dest('./server/views'));
+
+});
